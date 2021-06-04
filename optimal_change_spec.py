93ed1a996f0203462c1f8b5d12aaf8c9b45dbb77
Tim Gaston <Gman2105@users.noreply.github.com>
2021-06-04 21:31:27 +0000

    Tim's solution

--- optimal_change_spec.py
+++ optimal_change_spec.py
@@ -1,1 +1,27 @@
-# Write your unit tests here!
+import unittest
+
+from optimal_change import optimal_change
+
+# add tests to cover different edge cases
+
+class optimal_changeTestCase(unittest.TestCase):
+    """Tests for optimal_change.py"""
+    
+    def test_1(self):
+        self.assertEqual(optimal_change(99, 100), 1)
+        
+    def test_2(self):
+        self.assertEqual(optimal_change(45, 100), 55)
+    
+    def test_3(self):
+        self.assertNotEqual(optimal_change(99.99, 100), 0.01)
+        
+    def test_4(self):
+        self.assertEqual(optimal_change(100, 100), 0)
+    
+    def test_5(self):
+        self.assertTrue(optimal_change(100, 100) == 'No Change')
+        
+     # Your tests will go here
+if __name__ == '__main__':
+    unittest.main() 
